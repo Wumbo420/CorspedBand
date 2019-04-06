f0a2f6277d470eb68684d20e5efcfb9f2dcce540
Brandon Dawson <brandon2d@hotmail.com>
2019-04-05 23:31:55 -0400

    added widgets and pluings

--- js/image.js
+++ js/image.js
@@ -1,3 +1,4 @@
+/*image preloading*/
 const links = document.getElementsByTagName("main_img");
 let i, link, image;
 for ( i = 0; i < links.length; i++){
@@ -13,3 +14,30 @@ for ( x = 0; x < icons.length; x++){
     ima = new Image();
     image.src = icons.href;
 }
+/*this plugin allows image zoom compatibility*/
+$(document).ready(function() {
+	$('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+});
