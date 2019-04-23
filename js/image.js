00937de3587fdc0903eef90458dbab7e3ab4ea2a
Brandon Dawson <brandon2d@hotmail.com>
2019-04-23 18:32:42 -0400

    ajax + json v1

--- js/image.js
+++ js/image.js
@@ -1,43 +1,41 @@
 /*image preloading*/
-const links = document.getElementsByTagName("main_img");
-let i, link, image;
-for ( i = 0; i < links.length; i++){
-    link = links[i];
-    image = new Image();
-    image.src = links.href;
-}
-
-const icons = document.getElementsByClassName(".ico");
-let x, icon, ima;
-for ( x = 0; x < icons.length; x++){
-    ico = icons[x];
-    ima = new Image();
-    image.src = icons.href;
-}
-/*this plugin allows image zoom compatibility*/
 $(document).ready(function() {
-	$('.zoom-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-				return element.find('img');
-			}
-		}
+	const links = document.getElementsByTagName("main_img");
+	const icons = document.getElementsByClassName(".ico");
+	let x, icon, ima;
+	let i, link, image;
+	for ( i = 0; i < links.length; i++){
+		link = links[i];
+		image = new Image();
+		image.src = links.href;
+	}
+	
+	for ( x = 0; x < icons.length; x++){
+		ico = icons[x];
+		ima = new Image();
+		image.src = icons.href;
+	}
 		
-	});
-});
+	$('#main_img').magnificPopup({
+		items: {
+			src: $('<div class="zoom-gallery">Dynamically created popup</div>'),
+			type: 'inline'
+		}
+	});  
+
+	$('.ajax-popup-link').magnificPopup({
+  		type: 'ajax'
+		});
+		var url = "https://api.flickr.com/services/feeds/photos_public.gne?%20id=171090944@N04&format=json&jsoncallback=?";
+    $.getJSON(url, function(data){
+        var html = "";
+    $.each(data.items, function(i,item){
+
+            html =html.replace("https://api.flickr.com/services/feeds/photos_public.gne?%20id=171090944@N04&format=json&jsoncallback=?");
+            html += "<a href=" + item.media.m + " title=" + item.title + " rel=lightbox[vecta]>"; 
+            html += "<img src=" + item.media.m +" width=120 height=100>"; 
+            html += "</a>";        
+        });
+        $('#new_band').html(html); 
+    });
+  }); 
--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ $( function() {
   $( document ).tooltip({
     track: true
   });
-} );
+});
     //https://jqueryui.com/tooltip/
 // Example 1: From an element in DOM
-
